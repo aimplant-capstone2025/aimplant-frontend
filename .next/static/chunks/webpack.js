a6c2f19959f42d4fc0a21193ac3a77b5f4744e32
ffahf <fahfi.niti@gmail.com>
2025-03-04 15:19:24 +0700

    remove unuse

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a78856a63d2a6088"; }
+/******/ 		__webpack_require__.h = function() { return "d5d56103da40da59"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
